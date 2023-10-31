cae7a15fdcb760d87b27802a449d40b7a6be7a29
Tony <78621454+Tony-njiru@users.noreply.github.com>
2023-10-31 18:24:09 +0300

    This collaborates with todo.tsx for better results

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './App.css'; // Import your CSS file if you have one
+import './App.css'; 
 
 const App = () => {
   const [todos, setTodos] = useState([]);
@@ -8,7 +8,7 @@ const App = () => {
   const addTodo = () => {
     if (newTodo) {
       setTodos([...todos, { title: newTodo, completed: false }]);
-      setNewTodo(''); // Reset the input field
+      setNewTodo(''); // Resets the input field
     }
   };
 
