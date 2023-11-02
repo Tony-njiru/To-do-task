aa3f417f1054395e5a37e1e1db92cd221f6f422d
Tony <78621454+Tony-njiru@users.noreply.github.com>
2023-11-02 15:24:14 +0300

    Update LocalStorage.js
    Update of local storage

--- src/components/LocalStorage.js
+++ src/components/LocalStorage.js
@@ -1,32 +1,11 @@
 
-export const LocalStorage = {
-    // Get data from local storage
-    get: (key, defaultValue) => {
-      try {
-        const value = localStorage.getItem(key);
-        return value ? JSON.parse(value) : defaultValue;
-      } catch (error) {
-        console.error('Local storage get error:', error);
-        return defaultValue;
-      }
-    },
-  
-    // Set data in local storage
-    set: (key, value) => {
-      try {
-        localStorage.setItem(key, JSON.stringify(value));
-      } catch (error) {
-        console.error('Local storage set error:', error);
-      }
-    },
-  
-    // Remove data from local storage
-    remove: (key) => {
-      try {
-        localStorage.removeItem(key);
-      } catch (error) {
-        console.error('Local storage remove error:', error);
-      }
-    },
-  };
-  
+const LOCAL_STORAGE_KEY = 'todos';
+
+export function saveToLocalStorage(key, value) {
+  localStorage.setItem(key, JSON.stringify(value));
+}
+
+export function getFromLocalStorage(key, defaultValue) {
+  const storedValue = localStorage.getItem(key);
+  return storedValue ? JSON.parse(storedValue) : defaultValue;
+}
